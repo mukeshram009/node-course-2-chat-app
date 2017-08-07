519d5f1f9db1eedae7024429b776525d301fbf26
mukesh <mukesh@mukeshs-MacBook-Air.local>
2017-08-07 12:22:45 +0530

    created own http server and integrated socket.io into it

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+//listening to event
+socket.on('connect',function(){
+  console.log('connected to the server');
+
+  //emitting createEmail event as soon as connectedto the server.
+  socket.emit('createMessage',{
+    from: 'mukesh',
+    text: 'to the server'
+  });
+});
+
+socket.on('newMessage',function(email){
+  console.log('New Message',email);
+});
+
+socket.on('disconnect',() => {
+  console.log('disconnected from the server');
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');
-const socketIO = require('socket.io');
-
+const http = require('http');//in built module in node for making http request
+const socketIO = require('socket.io');//for making easy to use websockets.
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+//creating our own http server rather than using one in express to integrate socket.io
 var server = http.createServer(app);
+//here integrated socket.io into our own http server
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
@@ -16,12 +17,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {//this socket ~ socket in index.html but represents individual socket
   console.log('new user connected');
 
+  //emitting event
+  socket.emit('newMessage',{
+    from: 'server@gmail.com',
+    text: 'Hey dude! its me server serving you',
+    createdAt: 123
+  });
+
+  //listen to the event
+  socket.on('createMessage',(message) => {
+    console.log('createMessage :',message);
+  });
+
+
+  //listening to disconnect event
   socket.on('disconnect', () => {
     console.log('user disconnected from the server');
   });
 });
 
-
+//since we are now using our own http server rather than the express one
+//we use our server var. created above (our http server). so using server.listen()
 server.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
